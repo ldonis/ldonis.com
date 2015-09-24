314e9b2725e792d03ed9c078d8698fd21701bfc7
ldonis <ldonis.emc2@gmail.com>
2015-09-23 21:05:52 -0600

    Add cv section

--- cv/html/cv.php
+++ cv/html/cv.php
@@ -4,12 +4,17 @@
 
         <div>
             <i>Experiencia profesional:</i>
+            <br>
             <p>Cinco años de experiencia en desarrollo de software, trabajando para diferentes empresas</p>
+            <br>
             <p>Actualmente estoy trabajando para una empresa internacional, entre mis tareas esta dar mantenimiento/mejoras
                 a una red wordpress con 26 sitios web, analisis y desarrollo de mejoras y nuevas caracterias para el core bancario
                 de la institución.</p>
+            <br>
             <p>Para mas detalles de mi trabajo pueden ver mis repositorios en <?php echo $html->link('github', 'https://github.com/ldonis/'); ?>.</p>
+            <br>
             <?php echo '<a href="http://ldonis.com/www/ldonis.com/cv_ldonis_lite201509.pdf">descargar cv</a>'; ?>    
+            <br>
         </div>
         
     </article>
