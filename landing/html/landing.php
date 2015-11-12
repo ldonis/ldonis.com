7a92dde41dc98314be2c3a85e9ca78ca5af0aab2
ldonis <ldonis.emc2@gmail.com>
2015-11-12 12:26:52 -0600

    Adding atom

--- landing/html/landing.php
+++ landing/html/landing.php
@@ -39,13 +39,41 @@
             embodied in a small file compiled.
         </p>
 
-            <?php
-            /*
+    </article>
+
+    <article id="science" class="rblock columns-02">
+
         <div>
-            <h2>Science was my first love</h2>
+
+            <div id='atom_content'>
+                <div class='atom'>
+                    <div class="proton"></div>
+                    <div class="electron_1">
+                        <div></div>
+                    </div>
+                    <div class="electron_2">
+                        <div></div>
+                    </div>
+                    <div class="electron_3">
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-             */
-            ?>
+
+        <div>
+
+            <h2>Science, my first love</h2>
+            <p>
+                Science provide a set of tools to understand how the universe works.<br>
+                That is exciting, we can use mathematics to understand the movement
+                of one single particle to the all galaxies in the universe, all
+                that was achieved thanks to the curiosity and the reasoning capacity
+                of the humanity.
+            </p>
+
+        </div>
 
     </article>
 
