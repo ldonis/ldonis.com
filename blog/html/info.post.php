aace4ec1b3057d1b7b7b36797159b8936fbebf00
ldonis <ldonis.emc2@gmail.com>
2016-06-10 21:28:49 -0600

    back to the simple and clean...

--- blog/html/info.post.php
+++ blog/html/info.post.php
@@ -6,11 +6,6 @@ return array(
             'title' => 'Ciencia, más que una palabra',
             'date' => 'Junio 06, 2016',
             'excerpt' => 'Ciencia es un conjunto de conocimientos obtenidos mediante la observacion, razonamiento y experimentacion en ambitos especificos a partir de los cuales se generan preguntas, se construyen hipotesis se deducen principios y se elaboran leyes utilizando para ello el metodo cientifico. '
-        ),
-        'albert-einstein' => array(
-            'title' => 'Albert Einstein, el hombre que lo cambio todo',
-            'date' => '02/01/2016',
-            'excerpt' => 'super fan'
         )
 
 );
