da786881a53f4d73300982397501d16bc0a9d451
ldonis <ldonis.emc2@gmail.com>
2016-07-03 20:14:17 -0600

    ResponsiveCat post

--- blog/html/info.post.php
+++ blog/html/info.post.php
@@ -2,6 +2,12 @@
 
 return array(
 
+        'responsive-cat-mi-libreria-css' => array(
+            'title' => 'Responsive Cat, Mi libreria CSS',
+            'date' => 'Julio 03, 2016',
+            'excerpt' => 'Responsive Cat es un conjunto de librerias CSS diseñadas y desarrolladas 100% por mi; Este proyecto surgio como resultado de la insatisfactoria busqueda del framework dorado que se adaptara a todas mis necesidades referentes al desarrollo web.'
+        ),
+
         'ciencia-mas-que-una-palabra' => array(
             'title' => 'Ciencia, más que una palabra',
             'date' => 'Junio 27, 2016',
